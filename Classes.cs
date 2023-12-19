d94b17bb986b5826333e35b076c8f7e5fb8656a2
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2023-12-19 10:07:43 +0000

    complete key transeferal

--- Classes.cs
+++ Classes.cs
@@ -7,7 +7,7 @@ class Message
     public string ChannelID {get; set;}
     public string UserID {get; set;}
     public string UserName {get; set;}
-    public Double Time {get; set;}
+    public double Time {get; set;}
     public string Text {get; set;}
     public string IV {get; set;}
 }
